eb79be8f9329b2112115529ce7486b9b68ebb1bc
matthewschroeder05 <41975011+matthewschroeder05@users.noreply.github.com>
2018-12-05 14:29:19 -0800

    Add files via upload
    added functionality to delete a list. does security check

--- mylists.php
+++ mylists.php
@@ -27,6 +27,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 <html>
 <head>
 	<title>My Lists</title>
+	<script src="lists.js"></script>
 	<link rel="stylesheet" type="text/css" href="liststyle.css">
 </head>
 <body>
@@ -36,7 +37,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 			while($row = mysqli_fetch_assoc($result)) {
 				print "<div class=\"list-container\">";
 				$items = explode(",", $row['LIST']);
-				print "<div class=\"listtitle\">" . $row['TITLE'] . "<div class=\"title\"><a href=\"editlist.php?list=" . $row['ID'] . "\">edit</a></div></div><br>";
+				print "<div class=\"listtitle\">" . $row['TITLE'] . "<div class=\"title\"><a href=\"editlist.php?list=" . $row['ID'] . "\">edit</a> <a href=\"deletelist.php?list=" . $row['ID'] . "\">delete</a></div></div><br>";
 				$count = 0;
 				foreach($items as $item) {
 					($count % 2 != 0) ? print "<div class=\"listitem\">" . $item . "</div>" : print "<div class=\"mylisteven\">" . $item . "</div>";
